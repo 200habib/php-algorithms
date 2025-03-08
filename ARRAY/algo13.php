098381b1302f4512af4cc936735ce32ec0eab51a
el <elhabib20benaboud@gmail.com>
2025-03-09 00:06:56 +0100

    update

--- ARRAY/algo13.php
+++ ARRAY/algo13.php
@@ -2,14 +2,16 @@
 
 // "What is the largest number in nums?".
 
-$E = [10, 5, 8];
-$l = 0;
-for ($i=0; $i<count($E); $i++) {
-    
-	if ($l < $E[$i]) {
-	$l = $E[$i];	
-echo $l . "\n";
-}
-echo "finale: " . $l . "\n";
+<?php
+$numbers = [10, 5, 8];
+if (empty($numbers)) die("void !"); 
 
+$maxNumber = $numbers[0];
+
+foreach ($numbers as $num) {
+    if ($num > $maxNumber) {
+        $maxNumber = $num;
+    }
 }
+
+echo "Max: $maxNumber";
